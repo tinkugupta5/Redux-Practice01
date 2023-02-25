c5db3268a4719e799c7c1b0c05df1d2b1021c2fc
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-02-25 16:47:39 +0530

    add dependencies

--- src/components/store.js
+++ src/components/store.js
@@ -1,1 +1,13 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const rootReducer = combineReducers({});
+
+const intialState = {};
+const middleware = [thunk];
+const store = createStore(
+  rootReducer,
+  intialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
