e5643b2f34fb675f750a40d84ac11c723f439819
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-02-26 00:39:05 +0530

    add userconstand to reducer

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,10 @@
+import {
+  GET_USERS_REQUEST,
+  GET_USERS_SUCCESS,
+  GET_USERS_FAIL,
+} from "../constansts/userConstants";
+export const userReducer = (state = { users: [] }, actions) => {
+  switch (actions.type) {
+    case:GET_USERS_REQUEST:
+  }
+};
