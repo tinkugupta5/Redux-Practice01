d60ececeff371908f536464b836e8881ece7f9c0
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-03-02 21:40:01 +0530

    adding these files

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { userReducer } from "./redux/reducers/userReducer";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  userList: userReducer,
+});
 
 const intialState = {};
 const middleware = [thunk];
